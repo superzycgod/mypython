e54a6d992f7deacee85943ac547241ff7ee10830
zhangyanchao <zhangyanchao@senyint.com>
2020-11-30 15:44:01 +0800

    python3.4.4

---
+++ DynamicEmoticon/install34.bat
@@ -0,0 +1,6 @@
+rem  pip==19.1
+easy_install --find-links=./libs34/ pip==19.1
+
+rem install imageio
+pip install --no-index --find-links=./libs34/ imageio==2.6.0
+pause
---
+++ DynamicEmoticon/unintall34.bat
@@ -0,0 +1,4 @@
+pip uninstall -y imageio
+pip uninstall -y numpy
+pip uninstall -y Pillow
+python -m pip uninstall -y pip
